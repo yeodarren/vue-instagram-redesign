1f6b0248a597fa65dfd3fec010fb241599268501
yeodarren <71680726+yeodarren@users.noreply.github.com>
2021-01-07 20:36:02 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuelidate from 'vuelidate'
 import { auth } from './firebase/firebase'
+
+Vue.use(Vuelidate)
 
 Vue.config.productionTip = false
 
